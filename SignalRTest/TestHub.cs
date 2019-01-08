7efee1665b894805e528d2b582db78011a05646a
Nazrin Javadova <nazrin.cvd@gmail.com>
2019-01-08 15:21:41 +0400

    Add project files.

---
+++ SignalRTest/TestHub.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace signalrTestApp
+{
+    public class TestHub : Hub
+    {
+        public async Task Send(string input)
+        {
+            await Clients.All.SendAsync("Receive", input);
+        }
+
+        public async Task SendColor(bool isActive)
+        {
+            await Clients.All.SendAsync("ReceiveColor", isActive);
+        }
+
+        public async Task SendDelete(object inputs)
+        {
+            await Clients.All.SendAsync("ReceiveDelete", inputs);
+        }
+    }
+}
